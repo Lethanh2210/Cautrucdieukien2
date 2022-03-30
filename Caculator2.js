31b95a5068dae60f18f5421106abd7431aee9e90
LeThanh <kzxlmdtn888@gmail.com>
2022-03-30 20:11:24 +0700

    ..

---
+++ Caculator2.js
@@ -0,0 +1,11 @@
+function touch(value){
+    document.getElementById('input').value += value
+}
+function calculator() {
+    let input = document.getElementById('input').value;
+    let result = eval(input);
+    document.getElementById('input').value = result;
+}
+function clearTex() {
+    document.getElementById('input').value = '';
+}
